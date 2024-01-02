f8eb3b9629eb1993026abd41146c02e52d7f4276
Mateus Braga <braga.mateus16@gmail.com>
2024-01-02 16:44:33 -0300

    listando recurso via consulta JPQL

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,6 +1,9 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +13,12 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Mateus Braga");
-        proprietario1.setEmail("braga.mateus16@gmail.com");
-        proprietario1.setTelefone("61982653464");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Teste");
-        proprietario2.setEmail("teste@gmail.com");
-        proprietario2.setTelefone("9999999999");
-
-
-        return Arrays.asList(proprietario1, proprietario2);
+        TypedQuery query = manager.createQuery("from Proprietario", Proprietario.class);
+        return query.getResultList();
     }
 }
